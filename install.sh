edd5e789ff6aad55b55ab68f6ec79d1812b68ebd
Gabriele Lippi <gabriele@lippi.net>
2019-02-09 18:14:45 +0100

    Error in pointing to file like it was a directory

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@
 # create dict directory inside ./local/share/dict
 mkdir -p $HOME/.local/share/dict
 
-ln -sf ./tachyons.txt $HOME/.local/share/dict/tachyons.txt
+ln -sf tachyons.txt $HOME/.local/share/dict/tachyons.txt
